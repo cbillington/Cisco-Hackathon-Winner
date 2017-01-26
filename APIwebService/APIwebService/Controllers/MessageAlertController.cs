00657d26481d0cd0367e4659a627ae0eac8e104b
750118 <750118@ICTVM-FQQ06UJG2>
2017-01-26 11:31:24 -0700

    working response API

--- APIwebService/APIwebService/Controllers/MessageAlertController.cs
+++ APIwebService/APIwebService/Controllers/MessageAlertController.cs
@@ -11,18 +11,19 @@ namespace APIwebService.Controllers
     public class MessageAlertController : ApiController
     {
 
-        public int GetMessageAlert()
+        public IHttpActionResult GetMessageAlert()
         {
-            return 55; 
+            return Ok("200");
         }
 
        [HttpPost]
         public IHttpActionResult PostMessageAlert( [FromBody] MessageAlert alert)
         {
+            
 
             //https://qcwtgexljw.localtunnel.me
             HttpClient client = new HttpClient();
-            string baseAddress = "https://api.ciscospark.com/v1/messages";
+            string baseAddress = "https://api.ciscospark.com/v1/messages/";
             client.BaseAddress = new Uri(baseAddress);
 
             // Add an Accept header for JSON format.
@@ -32,13 +33,9 @@ namespace APIwebService.Controllers
             //adding authentication
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue ("Bearer", "Yzk0ZTNjZTktYzk5Mi00NzlkLThjZjEtOTQ1OTM4ODk1ZDA4NjRhMWNmNGQtNDJj");
 
-      
-      
-
-
-
             // List data response.
             HttpResponseMessage response = client.GetAsync(alert.data.id).Result;  // Blocking call!
+
             if (response.IsSuccessStatusCode)
             {
                 // Parse the response body. Blocking!
@@ -49,6 +46,22 @@ namespace APIwebService.Controllers
                     //therefore we grab the whole information
                     var message = response.Content.ReadAsAsync<Message>().Result;
 
+                    string postAddress = "https://api.ciscospark.com/v1/messages/";
+                    HttpClient postClient = new HttpClient();
+                    postClient.BaseAddress = new Uri(postAddress);
+                    postClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    //adding authentication
+                    postClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "Yzk0ZTNjZTktYzk5Mi00NzlkLThjZjEtOTQ1OTM4ODk1ZDA4NjRhMWNmNGQtNDJj");
+
+                    var postContent = new FormUrlEncodedContent(new[]
+                    {
+                new KeyValuePair<string, string>("toPersonId", "Y2lzY29zcGFyazovL3VzL1BFT1BMRS9hYzE5OTI2Mi1hNTg2LTQ3ZmUtYjU2OS05NDE5NDk3ZGRiN2Y"),
+                new KeyValuePair<string, string>("text", message.text)
+            });
+
+                    HttpResponseMessage postResponse = postClient.PostAsync("", postContent).Result;
 
                     return Ok(message.text);
                   
